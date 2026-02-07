fca8d6b26f9606279e7f17befaf039189e40091d
YuneArts <ThatTastedPurple1@gmail.com>
2026-02-07 00:34:49 -0800

    Power Minigame mostly complete.
    Implemented the moving bar with randomized speed and safespots, and button to click on to complete minigame.

---
+++ Assets/Scripts/PowerMinigameBar.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PowerMinigameBar : MonoBehaviour
+{
+    [SerializeField] private Transform pointA, pointB;
+    [SerializeField] private RectTransform safeZone;
+    private bool isPlaying;
+    public float moveSpeed;
+
+    private float direction;
+    private RectTransform barTransform;
+    private Vector3 targetPoisition;
+    void Start()
+    {
+        barTransform = GetComponent<RectTransform>();
+        targetPoisition = pointB.position;
+        ShufflePowerMinigame();
+        isPlaying = true;
+    }
+
+    void Update()
+    {
+        if (isPlaying)
+        {
+            barTransform.position = Vector3.MoveTowards(barTransform.position, targetPoisition, moveSpeed * Time.deltaTime);
+
+            if (Vector3.Distance(barTransform.position, pointA.position) < 0.1f)
+            {
+                targetPoisition = pointB.position;
+                direction = 1f;
+            }
+            else if (Vector3.Distance(barTransform.position, pointB.position) < 0.1f)
+            {
+                targetPoisition = pointA.position;
+                direction = -1f;
+            }
+        }
+    }
+
+    public void PowerButtonClick()
+    {
+        if (isPlaying)
+        {
+            StartCoroutine(CheckSuccess());
+        }
+    }
+
+
+    void ShufflePowerMinigame()
+    {
+        safeZone.sizeDelta = new Vector2(45f, Random.Range(35f, 80f));
+        moveSpeed = Random.Range(150f, 300f);
+        safeZone.position = new Vector2(safeZone.position.x, Random.Range(pointB.position.y, pointA.position.y));
+    }
+
+    private IEnumerator CheckSuccess()
+    {
+        isPlaying = false;
+
+        if (RectTransformUtility.RectangleContainsScreenPoint(safeZone, barTransform.position, null))
+        {
+            Debug.Log("Success! +2 Power");
+        }
+        else
+        {
+            Debug.Log("Failed. +1 Power");
+        }
+        yield return null;
+    }
+    
+}
