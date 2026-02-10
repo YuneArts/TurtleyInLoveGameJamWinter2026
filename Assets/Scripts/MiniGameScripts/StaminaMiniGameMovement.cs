059ebc5a6046391ec1e385cc6056102819be9079
YuneArts <ThatTastedPurple1@gmail.com>
2026-02-09 16:28:41 -0800

    Updated Tile Movement for Stamina MiniGame
    Integrated player movement to work with new input system to have tile based movement in the minigame. Added PlayerInput component to Player game object and NonWalkable layer to editor.

--- Assets/Scripts/MiniGameScripts/StaminaMiniGameMovement.cs
+++ Assets/Scripts/MiniGameScripts/StaminaMiniGameMovement.cs
@@ -1,15 +1,28 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 
 public class StaminaMiniGameMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private LayerMask nonwalkLayer;
+    [SerializeField] private PlayerInput playerInput;
+    private InputAction moveInput;
     public bool isMoving, levelComplete;
-    private Vector2 input;
+    private Vector2 moveValue;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        moveInput = playerInput.actions.FindAction("Movement");
+    }
+
+    private void OnDisable()
     {
         
     }
@@ -21,8 +34,46 @@ public class StaminaMiniGameMovement : MonoBehaviour
         {
             if(!isMoving)
             {
+                moveValue = moveInput.ReadValue<Vector2>();
+
+                if(moveValue != Vector2.zero)
+                {
+                    var targetPos = transform.position;
+                    targetPos.x += moveValue.x;
+                    targetPos.y += moveValue.y;
+                    Debug.Log("Horizontal: " + moveValue.x + ", Vertical: " + moveValue.y);
+                    if(isWalkable(targetPos))
+                    {
+                        StartCoroutine(MoveTile(targetPos));
+                    }
+                }
                 
             }
         }
+    }
+
+    IEnumerator MoveTile(Vector3 targetPos)
+    {
+        isMoving = true;
+
+        while((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+
+    private bool isWalkable(Vector3 targetPos)
+    {
+        if(Physics2D.OverlapCircle(targetPos, 0.2f, nonwalkLayer) != null)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
